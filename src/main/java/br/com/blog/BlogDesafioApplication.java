b7920e6901c60a7f47218450540923862175fa2a
carlosjrs006 <carlosjrs06.cj@gmail.com>
2024-01-30 03:20:11 -0300

    FEATURE - criação do projeto, configuração de banco, endpoints para o blog e etc..

--- src/main/java/br/com/blog/BlogDesafioApplication.java
+++ src/main/java/br/com/blog/BlogDesafioApplication.java
@@ -3,11 +3,10 @@ package br.com.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableJpaRepositories
 public class BlogDesafioApplication {
 
